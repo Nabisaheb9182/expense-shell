ccd439cd9a13749ef6744d632c45bbaf0d309a83
Nabi <nabisaheb7780@gmail.com>
2024-03-30 17:42:24 +0530

    Expense Project Setup

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 log_file=/tmp/expense.log
-color="\e[32m"
+color="\e[33m"
 
-echo -e "${color} Installing Nginx \e[0m"
+echo -e "$color Installing Nginx \e[0m"
 dnf install nginx -y &>>$log_file
 
 echo -e "${color} Copy Expense Config file \e[0m"
